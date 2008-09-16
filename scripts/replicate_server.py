6a1300e80c0cd40165073859c2363fee7dbc5b77
rplante <rplante@git.lsstcorp.org>
2008-09-16 16:29:30 +0000

    replicate_server.py: bug fix: dest dir check error (again)

--- scripts/replicate_server.py
+++ scripts/replicate_server.py
@@ -37,7 +37,7 @@ def main():
     if not os.access(fromstack, (os.R_OK|os.X_OK)):
         raise RuntimeError("%s: directory not readable/descendable" % fromstack)
         
-    if not os.path.exists(tostack):
+    if os.path.exists(tostack):
         if opts.new:
             raise RuntimeError("%s: destination directory already exists")
         elif not os.path.isdir(tostack) or \
