e07f1db3a01af94901e930ca77585e4b7ceadbb2
Joshua Hoblitt <josh@hoblitt.com>
2017-03-14 14:02:37 -0700

    bump EUPS version to 2.1.2

--- scripts/newinstall.sh
+++ scripts/newinstall.sh
@@ -25,7 +25,7 @@ set -e
 # Note to developers: change these when the EUPS version we use changes
 #
 
-EUPS_VERSION=${EUPS_VERSION:-2.1.1}
+EUPS_VERSION=${EUPS_VERSION:-2.1.2}
 
 EUPS_GITREV=${EUPS_GITREV:-""}
 EUPS_GITREPO=${EUPS_GITREPO:-"https://github.com/RobertLuptonTheGood/eups.git"}
