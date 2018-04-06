72f5fea6a23dbbc13a06276f316b1bb085ff315d
Joshua Hoblitt <josh@hoblitt.com>
2018-04-06 08:45:00 -0700

    eups build also requires at least: which perl awk sed

--- scripts/newinstall.sh
+++ scripts/newinstall.sh
@@ -798,7 +798,7 @@ n8l::install_eups() {
 	local eups_build_dir="$LSST_HOME/_build"
 
 	# make is absent from many minimal linux images
-	n8l::require_cmds make "${CC:-cc}"
+	n8l::require_cmds make "${CC:-cc}" which perl awk sed
 
 	if ! ( set -e
 		mkdir "$eups_build_dir"
