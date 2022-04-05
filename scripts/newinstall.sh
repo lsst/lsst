872cf931c6472d4be84022cabf02545a7851f1dd
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2022-04-05 15:05:48 -0700

    backport lsstinstall and mamba/newinstall.sh

--- scripts/newinstall.sh
+++ scripts/newinstall.sh
@@ -18,7 +18,7 @@ LSST_EUPS_USE_EUPSPKG=${LSST_EUPS_USE_EUPSPKG:-true}
 LSST_MINICONDA_VERSION=${LSST_MINICONDA_VERSION:-py38_4.9.2}
 # this git ref controls which set of conda packages are used to initialize the
 # the default conda env defined in scipipe_conda_env git package (RFC-553).
-LSST_SPLENV_REF=${LSST_SPLENV_REF:-${LSST_LSSTSW_REF:-0.7.0}}
+LSST_SPLENV_REF=${LSST_SPLENV_REF:-${LSST_LSSTSW_REF:-0.8.0}}
 LSST_MINICONDA_BASE_URL=${LSST_MINICONDA_BASE_URL:-https://repo.continuum.io/miniconda}
 LSST_CONDA_CHANNELS=${LSST_CONDA_CHANNELS:-"conda-forge"}
 LSST_CONDA_ENV_NAME=${LSST_CONDA_ENV_NAME:-lsst-scipipe-${LSST_SPLENV_REF}}
@@ -506,6 +506,9 @@ n8l::miniconda::lsst_env() {
 	(
 		set -Eeo pipefail
 
+		# install mamba to speed up environment creation
+		$cmd conda install -c conda-forge -y mamba
+
 		args=()
 		args+=('create')
 		args+=('-y')
@@ -527,7 +530,7 @@ n8l::miniconda::lsst_env() {
 			args+=("rubin-env=${ref}")
 		fi
 
-		$cmd conda "${args[@]}"
+		$cmd mamba "${args[@]}"
 
 		# Update rubin-env to latest build of specified version
 		if [[ "$ref" == [dsvw]* ]]; then
